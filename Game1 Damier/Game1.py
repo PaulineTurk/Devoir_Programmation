769fa52ff4cb0645600e1955c7a3a51422be584a
Kexin Li <kexin.li@agroparistech.fr>
2022-04-07 11:54:46 +0200

    la bonne version du game1

--- Game1 Damier/Game1.py
+++ Game1 Damier/Game1.py
@@ -22,7 +22,6 @@ class MyLayout(BoxLayout):
         global GRID, SIZE
         GRID = [[choice([0, 1]) for x in range(SIZE)] for y in range(SIZE)]
         self.mywidge.color_canvas()
-       
 
 
 
@@ -34,6 +33,7 @@ class MyWidget(Widget):
         if not self._keyboard:
             return
         self._keyboard.bind(on_key_down=self.on_keyboard_down)
+        Window.size = (800, 700)
 
     def color_canvas(self):   
         """
@@ -44,30 +44,20 @@ class MyWidget(Widget):
         """
         global GRID, SIZE
         cellheight = self.height//SIZE
-        cellwidth = cellheight    
-        start = 0
-        x = start
-        y = start
-        self.canvas.clear()
-        
+        cellwidth = cellheight
+
         with self.canvas:
             for line in range(SIZE):
                 for column in range(SIZE):
-                    if GRID[line][column]== 1:
-                        Color(0,0,0)   
+                    if GRID[line][column] == 1:
+                        Color(0, 0, 0)
                     else:
-                        Color(1,1,1) 
-                    Rectangle(pos = (x,y), size = (cellwidth, cellheight ))
-                    x+=cellwidth   # passer à la case de droite
-               
-                # retour à la première case de la ligne suivante 
-                x=start
-                y+=cellheight
-            self.img = Image(source = "pacman.png", pos = (0,cellheight *(SIZE-1)), size = (cellwidth, cellheight ))
+                        Color(1, 1, 1)
+                    Rectangle(pos=(cellwidth * line, cellheight * column), size=(cellwidth, cellheight))
+
+            self.img = Image(source="pacman.png", pos=(0, cellheight * (SIZE - 1)), size=(cellwidth, cellheight))
             self.grid = GRID
 
-        
-            
 
     def on_keyboard_down(self, keyboard, keycode, text, modifiers):
         """
@@ -79,10 +69,6 @@ class MyWidget(Widget):
         global SIZE
         cellheight = self.height//SIZE
         cellwidth = cellheight
-        longueur = len(self.grid)
-        self.grid_corrige = []
-        for indice in range(longueur):
-            self.grid_corrige.append(self.grid[longueur-1-indice])
 
         direction_blanc = {'left': (- cellwidth, 0),
                     'right': (cellwidth, 0),
@@ -94,20 +80,24 @@ class MyWidget(Widget):
                     'up': (0, - cellheight),
                     'down': (0, cellheight)}
 
-        if self.img.x//cellwidth in range(0,SIZE) and self.img.y//cellheight in range(0,SIZE):    
-            if self.grid_corrige[SIZE - 1-self.img.y//cellwidth][self.img.x//cellheight] == 1: 
+        if self.img.x//cellwidth in range(0, SIZE) and self.img.y//cellheight in range(0, SIZE):
+            if self.grid[self.img.x//cellheight][self.img.y//cellwidth] == 1:
                 direction = direction_noir
             else:
                 direction = direction_blanc
 
             if keycode[1] in direction:
-                self.img.x += direction[keycode[1]][0]
-                self.img.y += direction[keycode[1]][1]
+                if (self.img.x + direction[keycode[1]][0]) // cellwidth in range(0, SIZE) \
+                        and (self.img.y + direction[keycode[1]][1]) // cellheight in range(0, SIZE):
+                    self.img.x += direction[keycode[1]][0]
+                    self.img.y += direction[keycode[1]][1]
+                else:
+                    print('Vous allez sortir du plateau')
             else:
                 return False
             return True
         else:
-            print("Sortie du plateau")
+            print("Sorti du plateau !")
         
  
 class Game1App(App):       
